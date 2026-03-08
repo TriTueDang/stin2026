2acedd9a53930d0e37d4b061c135641ddecfaa9b
TriTueDang <145659919+TriTueDang@users.noreply.github.com>
2026-03-08 21:47:25 +0100

    Develop (#7)
    * Feature/frontend/issue 001 innit (#2)
    
    * innit vue frontend
    
    * update gitignore
    
    * Feature/backend/issue 001 innit (#3)
    
    * init spring boot
    
    * removed spring security for now to develop
    
    * api for frontend
    
    * communication with backend using rest (#4)
    
    * Feature/frontend/bugfix 001 vscode (#5)
    
    * removed vscode dir
    
    * update gitignore
    
    * added deploy to azure (#6)

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script setup>
+import axios from "axios"
+import { ref, onMounted } from "vue"
+
+const message = ref("")
+
+onMounted(async () => {
+  const response = await axios.get("http://localhost:8080/api/hello")
+  message.value = response.data
+})
+</script>
+
+<template>
+  <div>
+    {{ message }}
+    hello world
+  </div>
+</template>
