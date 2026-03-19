38976377fce37bfe09bf9373a6ad43eb8602fe3e
Tue Dang Tri <tri.tue.dang@tul.cz>
2026-03-19 13:17:40 +0100

    fixed for azure

--- backend/src/main/java/com/example/backend/service/ExchangeRateService.java
+++ backend/src/main/java/com/example/backend/service/ExchangeRateService.java
@@ -21,14 +21,14 @@ public class ExchangeRateService {
     public ExchangeRateResponse getRates(String base) {
 
         ExchangeRateResponse response = client.getRates(base);
-        storage.saveRates(response, "data/rates4.json");
+//        storage.saveRates(response, "data/rates4.json");
         return response;
 //        return storage.loadRates("data/rates-live.json");
     }
     public TimeframeResponse getTimeframeExchangeRates(String base, String startDate, String endDate) {
         TimeframeResponse response = client.getTimeframeExchangeRates(base, startDate, endDate);
 
-        storage.saveRates(response, "data/timeframe_rates.json");
+//        storage.saveRates(response, "data/timeframe_rates.json");
         return response;
 //        return storage.loadTimeframe("data/timeframe_rates.json");
     }
